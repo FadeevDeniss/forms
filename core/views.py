fb76e17af5fbf84fc37cc928fb738fdbea333a76
Denzzelindahouse <deniro-094@gmail.com>
2022-05-27 10:06:34 +0300

    Added views, create routing, form and component methods

--- core/views.py
+++ core/views.py
@@ -1,9 +1,61 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from modernrpc.core import rpc_method
+from rest_framework.parsers import JSONParser
+
+from core.models import CustomForm, FormFields
+from core.serializers import FormSerializer, FieldSerializer
 
 
-@rpc_method
-def form_api(request):
+class FormName:
     pass
 
 
+@rpc_method(entry_point='rpc')
+def form_api(request, id=0):
+    if request.method == 'POST':
+        form_data = JSONParser().parse(request)
+        form_serializer = FormSerializer(data=form_data)
+        if form_serializer.is_valid():
+            form_serializer.save()
+            return JsonResponse('Success', safe=False)
+        return JsonResponse('Error occured while adding field', safe=False)
+
+    elif request.method == 'PUT':
+        form_data = JSONParser().parse(request)
+        form = CustomForm.objects.get(form_id=form_data['form_id'])
+        form_serializer = FormSerializer(form, data=form_data)
+        if form_serializer.is_valid():
+            form_serializer.save()
+            return JsonResponse('Success', safe=False)
+        return JsonResponse('Error occured while updating field', safe=False)
+
+    elif request.method == 'DELETE':
+        form = CustomForm.objects.get(form_id=id)
+        form.delete()
+        return JsonResponse('Success', safe=False)
+
+
+@rpc_method(entry_point='rpc')
+def field_api(request, id=0):
+    if request.method == 'POST':
+        field_data = JSONParser().parse(request)
+        field_serializer = FieldSerializer(data=field_data)
+        if field_serializer.is_valid():
+            field_serializer.save()
+            return JsonResponse('Success', safe=False)
+        return JsonResponse('Error occured while adding field', safe=False)
+
+    elif request.method == 'PUT':
+        field_data = JSONParser().parse(request)
+        fields = FormFields.objects.get(field_id=field_data['field_id'])
+        field_serializer = FieldSerializer(fields, data=field_data)
+        if field_serializer.is_valid():
+            field_serializer.save()
+            return JsonResponse('Success', safe=False)
+        return JsonResponse('Error occured while updating field', safe=False)
+
+    elif request.method == 'DELETE':
+        fields = FormFields.objects.get(field_id=id)
+        fields.delete()
+        return JsonResponse('Success', safe=False)
--- ui/angular/src/app/app.component.ts
+++ ui/angular/src/app/app.component.ts
@@ -1,10 +1,82 @@
 import { Component } from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: '../../../../../forms/ui/angular/src/app/app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular';
+  title = 'angular10';
+
+  customForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    this.customForm = this.fb.group({
+      title: '',
+      fields: this.fb.array([this.createField()]),
+      choices: this.fb.array([this.createChoice()])
+    })
+  }
+
+  get choice(): FormArray {
+    return <FormArray> this.customForm.get('choices');
+  }
+
+  get field(): FormArray {
+    return <FormArray> this.customForm.get('fields')
+  }
+
+  addChoice() {
+    this.choice.push(this.createChoice());
+  }
+
+  addField() {
+    this.field.push(this.createField());
+  }
+
+  createField(): FormGroup {
+    return this.fb.group({
+      type: '',
+      name: '',
+      desc: '',
+    })
+  }
+
+  createChoice(): FormGroup {
+    return this.fb.group({
+      choice: ''
+    })
+  }
+
+  removeField(i: number) {
+    this.field.removeAt(i);
+  }
+
+  removeChoice(n: number) {
+    this.choice.removeAt(n);
+  }
+
+  onSubmit() {
+    console.log(this.customForm.value);
+
+  }
+
+  private selectedLink: string = "Select";
+
+  setradio(e: string): void {
+    this.selectedLink = e;
+  }
+
+  isSelected(name: string): boolean {
+    if (!this.selectedLink) { // if no radio button is selected, always return false so every nothing is shown
+      return false;
+    }
+
+    return (this.selectedLink === name); // if current radio button is selected, return true, else return false
+  }
 }
+
+
